38fc87aef4094ae5b8cadd65547a5c1cea863e30
Scott Barcik <oxooo5co77@impending.org>
2024-02-21 14:04:20 -0800

    Choose three random teams

--- Source/imp023/Private/Subsystem/TeamDataSubsystem.cpp
+++ Source/imp023/Private/Subsystem/TeamDataSubsystem.cpp
@@ -3,6 +3,8 @@
 
 #include "Subsystem/TeamDataSubsystem.h"
 
+#include "Algo/RandomShuffle.h"
+
 void UTeamDataSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
@@ -10,7 +12,7 @@ void UTeamDataSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	TeamData.Emplace(FTeamData{1, "Cjrostoamotu", "CJR", "Cjrostoamotu", FLinearColor::Gray});
 	TeamData.Emplace(FTeamData{2, "Lake Natron Crimson", "LNC", "LakeNatronCrimson", FLinearColor::Red});
 	TeamData.Emplace(FTeamData{3, "Oaxaca Azul Oscura", "OAO", "OaxacaAzulOscura", FLinearColor::Blue});
-	TeamData.Emplace(FTeamData{4, "Port Royale Kings", "PRK", "PortRoyaleKings", FLinearColor(1.0f, 0.0f, 1.0f, 1.0f)});
+	TeamData.Emplace(FTeamData{4, "Port Royale Kings", "PRK", "PortRoyaleKings", FLinearColor(0.75f, 0.0f, 0.75f, 1.0f)});
 	TeamData.Emplace(FTeamData{5, "Zelenyy Kharkiv", "KHA", "ZelenyyKharkiv", FLinearColor::Green});
 }
 
@@ -21,11 +23,13 @@ void UTeamDataSubsystem::Deinitialize()
 
 TArray<FTeamData> UTeamDataSubsystem::ChooseThree() const
 {
-	TArray<FTeamData> Result;
+	check(TeamData.Num() >= 3);
 
-	Result.Emplace(TeamData[0]);
-	Result.Emplace(TeamData[1]);
-	Result.Emplace(TeamData[2]);
+	TArray<FTeamData> Result = TeamData;
+
+	Algo::RandomShuffle(Result);
+
+	Result.RemoveAt(3, Result.Num() - 3);
 
 	return Result;
 }
