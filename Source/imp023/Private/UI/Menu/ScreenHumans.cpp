b2d1bb261f652b820206788569749c2350b2ea5a
Scott Barcik <oxooo5co77@impending.org>
2024-03-04 20:19:05 -0800

    Hook up ScreenHumans

--- Source/imp023/Private/UI/Menu/ScreenHumans.cpp
+++ Source/imp023/Private/UI/Menu/ScreenHumans.cpp
@@ -4,7 +4,17 @@
 #include "UI/Menu/ScreenHumans.h"
 
 #include "Actor/Menu/GameStateMenu.h"
+#include "Components/TextBlock.h"
 #include "UI/Manager/ScreenManager.h"
+
+static constexpr uint GMaxHumans = 8;
+
+void UScreenHumans::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	TrackedHumans = 1;
+}
 
 void UScreenHumans::OnInputMain(int const PlayerIndex)
 {
@@ -13,7 +23,7 @@ void UScreenHumans::OnInputMain(int const PlayerIndex)
 	AGameStateMenu* GameState = GetWorld()->GetGameState<AGameStateMenu>();
 	check(GameState);
 
-	GameState->SetExpectedHumans(1);
+	GameState->SetExpectedHumans(TrackedHumans);
 
 	ScreenManager->NavigateTo("TeamSelect");
 }
@@ -23,5 +33,36 @@ void UScreenHumans::OnInputBack(int const PlayerIndex)
 	Super::OnInputBack(PlayerIndex);
 
 	ScreenManager->NavigateTo("MainMenu");
+}
 
+
+void UScreenHumans::OnInputLeft(int const PlayerIndex)
+{
+	Super::OnInputLeft(PlayerIndex);
+
+	WrapTrackedHumans(-1);
+
+	UpdateText();
+}
+
+void UScreenHumans::OnInputRight(int const PlayerIndex)
+{
+	Super::OnInputRight(PlayerIndex);
+
+	WrapTrackedHumans(1);
+
+	UpdateText();
+}
+
+void UScreenHumans::UpdateText() const
+{
+	TextCount->SetText(FText::AsNumber(TrackedHumans));
+	TextHuman->SetText(TrackedHumans == 1 ? INVTEXT("Human") : INVTEXT("Humans"));
+}
+
+void UScreenHumans::WrapTrackedHumans(int const Amount)
+{
+	--TrackedHumans;
+	TrackedHumans = (TrackedHumans + GMaxHumans + Amount) % GMaxHumans;
+	++TrackedHumans;
 }
--- Source/imp023/Public/UI/Menu/ScreenHumans.h
+++ Source/imp023/Public/UI/Menu/ScreenHumans.h
@@ -6,6 +6,7 @@
 #include "UI/Manager/Screen.h"
 #include "ScreenHumans.generated.h"
 
+class UTextBlock;
 /**
  *
  */
@@ -15,6 +16,23 @@ class IMP023_API UScreenHumans : public UScreen
 	GENERATED_BODY()
 
 protected:
+	virtual void NativeConstruct() override;
+
+protected:
 	virtual void OnInputMain(int const PlayerIndex) override;
 	virtual void OnInputBack(int const PlayerIndex) override;
+	virtual void OnInputLeft(int const PlayerIndex) override;
+	virtual void OnInputRight(int const PlayerIndex) override;
+
+private:
+	void UpdateText() const;
+	void WrapTrackedHumans(int const Amount);
+
+private:
+	UPROPERTY()
+	UTextBlock* TextCount;
+	UPROPERTY()
+	UTextBlock* TextHuman;
+
+	uint TrackedHumans;
 };
